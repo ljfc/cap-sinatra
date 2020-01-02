030afcfdabae9f074f6258b60167bd1e2ba55398
Leo Critchley <leo@toucanbox.com>
2020-01-02 17:38:40 +0000

    Simple JSON API

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require 'sinatra'
+require 'json'
+require 'digest'
 
 class App < Sinatra::Base
-  get '/' do
-    'Changed from Sinatra'
+  before do
+    content_type :json
   end
 
-  get '/other' do
-    'Other'
+  get '/' do
+    { status: :ok }.to_json
+  end
+
+  get '/parent/:name' do
+    {
+      name: params[:name],
+      digest: Digest::SHA256.base64digest params[:name]
+    }.to_json
   end
 end
