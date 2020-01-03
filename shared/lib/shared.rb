177a9eea83f2282ca909e8d32f3d0210bc5f00a4
Leo Critchley <leo@toucanbox.com>
2020-01-03 10:43:36 +0000

    Basic shared gem

---
+++ shared/lib/shared.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'digest'
+
+module Shared
+
+  class Person
+    def initialize(name)
+      @name = name
+    end
+
+    def name
+      @name
+    end
+
+    def digest
+      @digest ||= Digest::SHA256.base64digest(self.name)
+    end
+
+    def to_json
+      @json ||= {
+        name: self.name,
+        digest: self.digest
+      }.to_json
+    end
+  end
+
+end
---
+++ shared/shared.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |spec|
+  spec.name = 'shared'
+  spec.version = '0.0.0'
+  spec.authors = ['Leo']
+  spec.email = ['leo@toucanbox.com']
+  spec.summary = 'An example of some shared logic'
+
+  spec.files = ['lib/shared.rb']
+  spec.require_paths = ['lib']
+end
